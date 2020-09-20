211b3b4058259f9c9f287dc1ab318b0e2cbebd72
martinyounghoonkim <hoons0131@gmail.com>
2020-09-20 20:40:34 +0900

    샘플 페이지 및 컨트롤러 추가

--- src/main/java/com/inflearn/springsecurity/form/SampleController.java
+++ src/main/java/com/inflearn/springsecurity/form/SampleController.java
@@ -1,5 +1,6 @@
 package com.inflearn.springsecurity.form;
 
+import java.security.Principal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,8 +9,30 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class SampleController {
 
     @GetMapping("/")
-    public String index (Model model) {
-        model.addAttribute("message", "hello Spring security");
+    public String index (Model model, Principal principal) {
+        if (principal == null) {
+            model.addAttribute("message", "hello Spring security");
+        } else {
+            model.addAttribute("message", "hello" + principal.getName());
+        }
+
         return "index";
+    }
+    @GetMapping("/info")
+    public String info (Model model) {
+        model.addAttribute("message", "information");
+        return "info";
+    }
+
+    @GetMapping("/dashboard")
+    public String dashboard (Model model, Principal principal) {
+        model.addAttribute("message", "Hello dashboard" + principal.getName());
+        return "dashboard";
+    }
+
+    @GetMapping("/admin")
+    public String admin (Model model, Principal principal) {
+        model.addAttribute("message", "Hello admin" + principal.getName());
+        return "admin";
     }
 }
