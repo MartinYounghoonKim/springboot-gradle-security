ad8d028b16c01a40bd4477a2a3707fa61ac4376f
martinyounghoonkim <hoons0131@gmail.com>
2020-09-20 21:26:08 +0900

    계정에 대한 환경 설정 추가

--- src/main/java/com/inflearn/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/inflearn/springsecurity/config/SecurityConfig.java
@@ -13,10 +13,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests()
                 .mvcMatchers("/", "info").permitAll()   // 인증 여부와 상관없이 항상 접근을 허용해줌
                 .mvcMatchers("/admin").hasRole("ADMIN") // ADMIN 권한을 가진 유저만 접근 허용해줌
-                .anyRequest().authenticated()                    // 인증만 된다면 접근 허용해줌
-                .and()                                           // method 체이닝을 and 로 꼭 연결 하지 않아도 된다.
-            .formLogin()
-                .and()
-            .httpBasic();
+                .anyRequest().authenticated();                   // 인증만 된다면 접근 허용해줌
+//                .and()                                         // method 체이닝을 and 로 꼭 연결 하지 않아도 된다.
+
+        http.formLogin();
+        http.httpBasic();
     }
 }
