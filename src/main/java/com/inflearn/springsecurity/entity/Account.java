5d3cd344a72f4d0fdafddc842b731cc69d1d2411
martinyounghoonkim <hoons0131@gmail.com>
2020-10-02 17:01:28 +0900

    formLogin 성공/실패에 대한 단위 테스트 추가

--- src/main/java/com/inflearn/springsecurity/entity/Account.java
+++ src/main/java/com/inflearn/springsecurity/entity/Account.java
@@ -6,13 +6,19 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Entity
 @Getter
 @Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Account {
     @Id @GeneratedValue(strategy = AUTO)
     private Long id;
--- src/test/java/com/inflearn/springsecurity/controller/AccountControllerTest.java
+++ src/test/java/com/inflearn/springsecurity/controller/AccountControllerTest.java
@@ -1,10 +1,14 @@
 package com.inflearn.springsecurity.controller;
 
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.inflearn.springsecurity.entity.Account;
+import com.inflearn.springsecurity.service.AccountService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +18,7 @@ import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -21,6 +26,9 @@ import org.springframework.test.web.servlet.MockMvc;
 public class AccountControllerTest {
     @Autowired
     MockMvc mockMvc;
+
+    @Autowired
+    AccountService accountService;
 
     @Test
     @WithAnonymousUser
@@ -54,5 +62,33 @@ public class AccountControllerTest {
         mockMvc.perform(get("/admin"))
                 .andDo(print())
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    @Transactional
+    public void 로그인_성공 () throws Exception {
+        String username = "martin";
+        String password = "1234";
+        createUser(username, password);
+        mockMvc.perform(formLogin().user(username).password(password))
+                .andExpect(authenticated());
+    }
+    @Test
+    @Transactional // 하나의 Test 클래스에 여러개의 유저를 생성하기 때문에, username unique 에러가 발생하기 때문에 Transactional 어노테이션을 추가하여, 실패 시 rollback 시킨다.
+    public void 로그인_실패 () throws Exception {
+        String username = "martin";
+        String password = "1234";
+        createUser(username, password);
+        mockMvc.perform(formLogin().user(username).password("12345"))
+                .andExpect(unauthenticated());
+    }
+
+    private void createUser(String username, String password) {
+        Account account = Account.builder()
+                .username(username)
+                .password(password)
+                .role("USER")
+                .build();
+        accountService.signUp(account);
     }
 }
