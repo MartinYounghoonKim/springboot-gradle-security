56e279d0ecab6f58fb64ab345680f721a3632278
martinyounghoonkim <hoons0131@gmail.com>
2020-10-03 17:20:56 +0900

    rememberMe 에 설정에 대한 설명 추가

--- core/src/main/java/com/inflearn/springsecuritycore/configure/SecurityConfigure.java
+++ core/src/main/java/com/inflearn/springsecuritycore/configure/SecurityConfigure.java
@@ -23,7 +23,8 @@ public class SecurityConfigure extends WebSecurityConfigurerAdapter {
         // rememberMe 기능 활성화
         http.rememberMe()
                 .rememberMeParameter("remember") // 기본값은 remember-me
-                .tokenValiditySeconds(60 * 60)   // 기본값은 14일
+                // 만약 -1로 설정한다면 사용자가 웹 브라우저 종료 시 함께 사라지는 세션 쿠키로 설정
+                .tokenValiditySeconds(60 * 60)   // 기본값은 14일.
                 .alwaysRemember(true)            // rememberMe 기능이 활성화되지 않아도 항상 실행
                 .userDetailsService(userDetailsService);
     }
