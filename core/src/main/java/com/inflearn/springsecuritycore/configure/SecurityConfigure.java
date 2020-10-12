770736f6e2c4dfa8a86fbf80fb2b5d4eb72aa494
martinyounghoonkim <hoons0131@gmail.com>
2020-10-12 16:46:30 +0900

    인메모리 사용자 추가

--- core/src/main/java/com/inflearn/springsecuritycore/configure/SecurityConfigure.java
+++ core/src/main/java/com/inflearn/springsecuritycore/configure/SecurityConfigure.java
@@ -2,6 +2,7 @@ package com.inflearn.springsecuritycore.configure;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -14,11 +15,24 @@ public class SecurityConfigure extends WebSecurityConfigurerAdapter {
     UserDetailsService userDetailsService;
 
     @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("user").password("{noop}1111").roles("USER");
+        auth.inMemoryAuthentication().withUser("admin").password("{noop}1111").roles("ADMIN");
+        auth.inMemoryAuthentication().withUser("sys").password("{noop}1111").roles("SYS");
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .anyRequest().authenticated();
         // formLogin 활성화
         http.formLogin();
+
+        http.authorizeRequests()
+            .antMatchers("/user").hasRole("USER")
+            .antMatchers("/admin/pay").hasRole("ADMIN")
+            .antMatchers("/admin/**").access("hasRole('ADMIN') or hasRole('SYS')")
+            .anyRequest().authenticated();
 
         // rememberMe 기능 활성화
         http.rememberMe()
--- core/src/main/java/com/inflearn/springsecuritycore/controller/SecurityController.java
+++ core/src/main/java/com/inflearn/springsecuritycore/controller/SecurityController.java
@@ -9,4 +9,17 @@ public class SecurityController {
     public String index () {
         return "home";
     }
+    @GetMapping("/user")
+    public String user () {
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public String admin () {
+        return "admin";
+    }
+    @GetMapping("/admin/pay")
+    public String adminPay () {
+        return "adminPay";
+    }
 }
